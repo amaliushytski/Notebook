1637d5ebf7be24b91304639f28bbc4349c0ddd5f
Alex <37873959+amaliushytski@users.noreply.github.com>
2022-12-15 15:14:40 +0100

    Add files via upload

---
+++ src/com/tc/nb/dal/exception/NotesDataParseException.java
@@ -0,0 +1,21 @@
+package com.tc.nb.dal.exception;
+
+public class NotesDataParseException extends Exception {
+
+	public NotesDataParseException(String message) {
+		super(message);
+	}
+
+	public NotesDataParseException() {
+		super();
+	}
+
+	public NotesDataParseException(Exception e) {
+		super(e);
+	}
+
+	public NotesDataParseException(String message, Exception e) {
+		super(message, e);
+	}
+
+}
