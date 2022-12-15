188eb49b47442993585c864d4dfcaacf560000f3
Alex <37873959+amaliushytski@users.noreply.github.com>
2022-12-15 15:12:11 +0100

    Update FileNotesDAO.java

--- src/com/tc/nb/dal/impl/FileNotesDAO.java
+++ src/com/tc/nb/dal/impl/FileNotesDAO.java
@@ -7,11 +7,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
-
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.tc.nb.dal.NotesDAO;
+import com.tc.nb.dal.exception.NotesDataParseException;
 import com.tc.nb.entity.Note;
 
 public class FileNotesDAO implements NotesDAO {
@@ -20,27 +21,38 @@ public class FileNotesDAO implements NotesDAO {
 
 	private static final String SOURCE_PATH = "/Users/a/my_notebook/src/resources/notes.txt";
 
-	public FileNotesDAO() {
+	private static final int CREATION_DATE = 0;
+
+	private static final int TITLE = 1;
+
+	private static final int CONTENT = 2;
+
+	private static final String EMPTY_STRING = "";
+
+	public FileNotesDAO() throws Exception {
+		BufferedReader reader = null;
+		FileReader fr = null;
 		try {
-			FileReader fr = new FileReader(SOURCE_PATH);
-			BufferedReader reader = new BufferedReader(fr);
+			fr = new FileReader(SOURCE_PATH);
+			reader = new BufferedReader(fr);
 			String newReadLine;
 
 			while ((newReadLine = reader.readLine()) != null) {
 				try {
-					String[] data = newReadLine.split(";");
-					String title = data[1];
-					String content = data[2];
-					LocalDateTime dateTime = LocalDateTime.parse(data[0]);
-					Note x = new Note(dateTime, title, content);
-					notes.add(x);
-				} catch (Exception e) {
-					continue;
+					if (newReadLine != EMPTY_STRING) {
+						String[] data = newReadLine.split(";");
+						LocalDateTime dateTime = LocalDateTime.parse(data[CREATION_DATE]);
+						Note x = new Note(dateTime, data[TITLE], data[CONTENT]);
+						notes.add(x);
+					}
+				} catch (DateTimeParseException e) {
+					throw new NotesDataParseException("Row can't be empty: " + e.getMessage());
 				}
 			}
-			reader.close();
 		} catch (IOException ex) {
 			System.out.println(ex.getMessage());
+		} finally {
+			reader.close();
 		}
 	}
 
