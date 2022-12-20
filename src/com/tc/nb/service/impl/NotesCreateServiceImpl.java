aa7ee85e9558abe32ad50273fbddcadeb5d99be5
Alex <37873959+amaliushytski@users.noreply.github.com>
2022-12-20 15:37:21 +0100

    Update NotesCreateServiceImpl.java

--- src/com/tc/nb/service/impl/NotesCreateServiceImpl.java
+++ src/com/tc/nb/service/impl/NotesCreateServiceImpl.java
@@ -1,21 +1,26 @@
 package com.tc.nb.service.impl;
 
-
 import com.tc.nb.dal.DAOProvider;
 import com.tc.nb.dal.NotesDAO;
+import com.tc.nb.dal.exception.DAOException;
 import com.tc.nb.entity.Note;
 import com.tc.nb.service.NotesCreateService;
+import com.tc.nb.service.exception.ServiceException;
 
-public class NotesCreateServiceImpl implements NotesCreateService{
-	
+public class NotesCreateServiceImpl implements NotesCreateService {
+
 	@Override
-	public void add(Note n) {
-		
-        DAOProvider provider = DAOProvider.getInstance();
-		
-        NotesDAO notesDAO = provider.getNotesDAO();
-        
-        notesDAO.save(n);
+	public void add(Note n) throws ServiceException {
+
+		DAOProvider provider = DAOProvider.getInstance();
+
+		NotesDAO notesDAO = provider.getNotesDAO();
+
+		try {
+			notesDAO.save(n);
+		} catch (DAOException e) {
+			throw new ServiceException();
+		}
 
 	}
 
