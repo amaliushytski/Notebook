ca1313d2014765f748f00db9a7e8181b318787ce
Alex <37873959+amaliushytski@users.noreply.github.com>
2022-12-20 15:36:44 +0100

    Update NotesFindServiceImpl.java

--- src/com/tc/nb/service/impl/NotesFindServiceImpl.java
+++ src/com/tc/nb/service/impl/NotesFindServiceImpl.java
@@ -1,38 +1,91 @@
 package com.tc.nb.service.impl;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-
 import com.tc.nb.dal.DAOProvider;
 import com.tc.nb.dal.NotesDAO;
-import com.tc.nb.dal.impl.FileNotesDAO;
+import com.tc.nb.dal.exception.DAOException;
 import com.tc.nb.entity.Note;
 import com.tc.nb.service.NotesFindService;
+import com.tc.nb.service.exception.ServiceException;
 
 public class NotesFindServiceImpl implements NotesFindService {
 
 	@Override
-	public List<Note> findByContent(String content) {
+	public List<Note> findByContent(String content) throws ServiceException {
 
 		DAOProvider provider = DAOProvider.getInstance();
 
 		NotesDAO notesDAO = provider.getNotesDAO();
 
-		List<Note> sourceList = notesDAO.load();
+		List<Note> sourceList;
+		try {
+			sourceList = notesDAO.load();
+		} catch (DAOException e) {
+			throw new ServiceException();
+		}
 
-		return null;
+		List<Note> foundData = new ArrayList<>();
+
+		for (Note Note : sourceList) {
+			if (Note.getContent().contains(content)) {
+				foundData.add(Note);
+			}
+		}
+
+		return foundData;
 	}
 
 	@Override
-	public List<Note> findByDate(Date date) {
-		
+	public List<Note> findByDate(LocalDateTime date) throws ServiceException {
+
 		DAOProvider provider = DAOProvider.getInstance();
-		
+
 		NotesDAO notesDAO = provider.getNotesDAO();
 
-		List<Note> sourceList = notesDAO.load();
+		List<Note> sourceList;
+		try {
+			sourceList = notesDAO.load();
+		} catch (DAOException e) {
+			throw new ServiceException();
+		}
 
-		return null;
+		List<Note> foundData = new ArrayList<>();
+
+		for (Note Note : sourceList) {
+
+			if (Note.getDate().equals(date)) {
+				foundData.add(Note);
+			}
+		}
+
+		return foundData;
+	}
+
+	@Override
+	public List<Note> findByDateRange(LocalDateTime dateFrom, LocalDateTime dateTo) throws ServiceException {
+
+		DAOProvider provider = DAOProvider.getInstance();
+
+		NotesDAO notesDAO = provider.getNotesDAO();
+
+		List<Note> sourceList;
+		try {
+			sourceList = notesDAO.load();
+		} catch (DAOException e) {
+			throw new ServiceException();
+		}
+
+		List<Note> foundData = new ArrayList<>();
+
+		for (Note note : sourceList) {
+			if (note.getDate().isAfter(dateFrom) & note.getDate().isBefore(dateTo)) {
+				foundData.add(note);
+			}
+		}
+
+		return foundData;
 	}
 
 }
