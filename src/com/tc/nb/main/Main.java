ef172132a79f18abe8a0e60688aaa24674671fac
Alex <37873959+amaliushytski@users.noreply.github.com>
2022-12-20 15:40:03 +0100

    Update Main.java

--- src/com/tc/nb/main/Main.java
+++ src/com/tc/nb/main/Main.java
@@ -1,33 +1,34 @@
 package com.tc.nb.main;
 
-
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 import com.tc.nb.dal.impl.FileNotesDAO;
 import com.tc.nb.entity.Note;
+import com.tc.nb.service.impl.NotesCreateServiceImpl;
+import com.tc.nb.service.impl.NotesFindServiceImpl;
+import com.tc.nb.service.impl.NotesViewServiceImpl;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-		FileNotesDAO y = new FileNotesDAO();
-		List<Note> notes = y.load();
-		
-		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-		LocalDateTime dateTime = LocalDateTime.parse("2010-11-09 11:44", formatter);
+		NotesFindServiceImpl service = new NotesFindServiceImpl();
+		//Find note by content
+		List<Note> notes = service.findByContent("Test");
+		//Find note by date range
+		List<Note> notes = service.findByDateRange(LocalDateTime.parse("2022-12-01T15:39:46.510000"),
+				LocalDateTime.parse("2022-12-02T15:39:46.510000"));
 
-		for (Note note : notes) {
-			System.out.println(note.getDate());
-			System.out.println(note.getContent());
-			System.out.println(note.getTitle());
-			System.out.println("-------------");
-		}
+		NotesViewServiceImpl view = new NotesViewServiceImpl();
 
-		y.save(new Note(dateTime,"Hello1","Test content1"));
-		
+		view.view(notes);
+
+		NotesCreateServiceImpl save = new NotesCreateServiceImpl();
+		//Add a new note
+		save.add(new Note(LocalDateTime.now(), "New text", "Test content added in main file"));
+
 	}
-
 }
