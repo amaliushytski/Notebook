6adeeb9514a5baf8dc6c5549d6aebd53990b9680
Alex <37873959+amaliushytski@users.noreply.github.com>
2022-12-20 15:33:52 +0100

    Update NotesFindService.java

--- src/com/tc/nb/service/NotesFindService.java
+++ src/com/tc/nb/service/NotesFindService.java
@@ -1,13 +1,16 @@
 package com.tc.nb.service;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
-
 import com.tc.nb.entity.Note;
+import com.tc.nb.service.exception.ServiceException;
 
 public interface NotesFindService {
-	
-	List<Note> findByContent(String content);
-	List<Note> findByDate(Date date);
+
+	List<Note> findByContent(String content) throws ServiceException;
+
+	List<Note> findByDate(LocalDateTime date) throws ServiceException;
+
+	List<Note> findByDateRange(LocalDateTime dateFrom, LocalDateTime dateTo) throws ServiceException;
 
 }
