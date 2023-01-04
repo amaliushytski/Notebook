80db2fa41dae561fd5f9a6fbb56fa04959206379
Alex <37873959+amaliushytski@users.noreply.github.com>
2023-01-04 12:08:50 +0100

    Update Main.java

--- src/com/tc/nb/main/Main.java
+++ src/com/tc/nb/main/Main.java
@@ -18,28 +18,9 @@ import com.tc.nb.view.impl.NotesViewImpl;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-//
-//		NotesFindServiceImpl service = new NotesFindServiceImpl();
-//
-//		List<Note> notes1 = service.findByContent("Test");
-//		List<Note> notes = service.findByDateRange(LocalDateTime.parse("2022-12-01T15:39:46.510"),
-//				LocalDateTime.parse("2022-12-16T15:39:46.510"));
-//
-//		NotesViewImpl view = new NotesViewImpl();
-//
-//		view.view(notes);
-//
-//		NotesCreateServiceImpl save = new NotesCreateServiceImpl();
-//
-//		save.add(new Note(LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS), "New text", "Test content added in main file"));
-//		
+	
 		Controller controller = new Controller();
-//		controller.executeTask("save New title from controller; Content from controller");
-//
-		// controller.executeTask("SEARCH_BY_TITLE text");
-//		controller.executeTask("SEARCH_BY_CONTENT test");
-//		controller.executeTask("SEARCH_BY_DATE 2022-12-01T15:39:46.511");
-//		controller.executeTask("SEARCH_BY_DATE_RANGE 2022-12-01T15:39:46.510 | 2022-12-01T15:39:46.510");
+		
 		NoteResponse response = controller.executeTask("save New title from controller; Content from controller");
 		controller.executeTask("VIEW " + response.getResponse());
 
@@ -54,30 +35,6 @@ public class Main {
 
 		response = controller.executeTask("SEARCH_BY_DATE_RANGE 2022-12-09T15:20:34.209 | 2022-12-29T15:20:34.209");
 		controller.executeTask("VIEW " + response.getResponse());
-
-		// service.findByDateRange(null, null);
-
-//		FileNotesDAO y = null;
-//		try {
-//			y = new FileNotesDAO();
-//		} catch (Exception e) {
-//			System.out.println(e.getMessage());
-//		}
-
-//		List<Note> notes = y.load();
-//
-//		LocalDateTime dateTime = LocalDateTime.parse("2012-11-14T11:44");
-//
-//		System.out.println(dateTime);
-
-//		for (Note note : notes) {
-//			System.out.println(note.getDate());
-//			System.out.println(note.getContent());
-//			System.out.println(note.getTitle());
-//			System.out.println("-------------");
-//		}
-
-//		y.save(new Note(LocalDateTime.now(), "New text", "Test content added in main file"));
 
 	}
 }
